c37f93f77e7c71be136089a4ddbca4774cfb1c01
Jeferson <jefeleal363@gmail.com>
2026-08-06 12:45:40 -0300

    feat: vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { inject } from '@vercel/analytics';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  inject();
+}
+
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
